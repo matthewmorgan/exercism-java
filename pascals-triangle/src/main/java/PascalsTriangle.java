2106756812ce20dd1096bd053d7209ef9c7a5d92
Matthew Morgan <matt@dulcetsoftware.com>
2015-10-05 08:06:48 -0400

    streamy triangles

--- pascals-triangle/src/main/java/PascalsTriangle.java
+++ pascals-triangle/src/main/java/PascalsTriangle.java
@@ -1,26 +1,28 @@
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class PascalsTriangle {
 
-    public static int[][] computeTriangle(int rowCount) {
+    public static Integer[][] computeTriangle(int rowCount) {
         if (rowCount < 0) throw new IllegalArgumentException();
         return generateRows(rowCount);
     }
 
-    private static int[][] generateRows(int rowCount){
-        int[][] rows = new int[rowCount][];
-        for (int ii = 0; ii < rowCount; ii++) {
-            int[] row = new int[ii + 1];
-            for (int jj = 0; jj < ii + 1; jj++) {
-                row[jj] = getCellValue(ii, jj);
-            }
-            rows[ii] = row;
-        }
-        return rows;
+    private static Integer[][] generateRows(int rowCount){
+        return IntStream.range(0, rowCount)
+                .mapToObj(ii -> {
+                    return IntStream.rangeClosed(0, ii)
+                            .mapToObj(jj -> getCellValue(ii, jj))
+                            .collect(Collectors.toList()).toArray(new Integer[ii]);
+                })
+                .collect(Collectors.toList()).toArray(new Integer[rowCount][]);
     }
 
     public static boolean isTriangle(int[][] triangle) {
-        return Arrays.deepEquals(triangle, computeTriangle(triangle.length));
+        Integer[][] computed = computeTriangle(triangle.length);
+        return IntStream.range(0, triangle.length)
+                .allMatch(ii -> IntStream.range(0,triangle[ii].length).allMatch(jj -> triangle[ii][jj] == computed[ii][jj]));
     }
 
     private static int getCellValue(int n, int k) {
@@ -28,10 +30,7 @@ public class PascalsTriangle {
     }
 
     private static int factorial(int n) {
-        int f = 1;
-        for (int i = 1; i <= n; i++) {
-            f *= i;
-        }
-        return f;
+        return IntStream.rangeClosed(1, n)
+                .reduce(1, (result, factor) -> result * factor);
     }
 }
