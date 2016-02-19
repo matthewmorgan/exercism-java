b4d41653b5fea0770364c549fee8576ca15217c5
Matthew Morgan <matt@dulcetsoftware.com>
2016-02-19 07:23:14 -0500

    Improve regex in WordCount to eliminate empty string check

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -3,12 +3,11 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public final class WordCount {
-    private final static Pattern onSpace = Pattern.compile("\\s");
+    private final static Pattern onSpace = Pattern.compile("\\s+");
     private final static String nonAlpha = "[^0-9A-Za-z\\s]";
 
     public final static Map<String, Integer> phrase(String input) {
         return onSpace.splitAsStream(input.replaceAll(nonAlpha, "").toLowerCase())
-//                .filter(word -> word.length() > 0)
-                .collect(Collectors.groupingBy(word -> word, Collectors.summingInt(word -> 1)));
+                .collect(Collectors.groupingBy(word -> word, Collectors.summingInt(w -> 1)));
     }
 }
