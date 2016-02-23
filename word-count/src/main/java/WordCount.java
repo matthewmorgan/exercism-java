5fa6e433768b412c9a7c8ce9b25a000209faf97f
Matthew Morgan <matt@dulcetsoftware.com>
2016-02-22 20:57:55 -0500

    Refactor for custom collector

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -1,5 +1,7 @@
 import java.util.Map;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public final class WordCount {
@@ -7,7 +9,15 @@ public final class WordCount {
     private final static String nonAlpha = "[^0-9A-Za-z\\s]";
 
     public final static Map<String, Integer> phrase(String input) {
-        return onSpace.splitAsStream(input.replaceAll(nonAlpha, "").toLowerCase())
-                .collect(Collectors.groupingBy(word -> word, Collectors.summingInt(w -> 1)));
+        return onSpace
+                .splitAsStream(cleaned(input))
+                .collect(toMapOfCountsByWord);
     }
+
+    private static final String cleaned(String input){
+        return input.replaceAll(nonAlpha, "").toLowerCase();
+    }
+
+    private static final Collector<String, ? ,Map<String, Integer>> toMapOfCountsByWord =
+            Collectors.groupingBy(String::new, Collectors.mapping(Function.identity(), Collectors.summingInt(word -> 1)));
 }
